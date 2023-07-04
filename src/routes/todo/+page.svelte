fabe8f605d8f0e2d1aef38ecbfc57881a7cda27f
KAME <zkamekyu@gmail.com>
2023-07-04 21:44:41 +0200

    Literally nothing changed here.

--- src/routes/todo/+page.svelte
+++ src/routes/todo/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-  import { onMount } from "svelte";
+    import { onMount } from "svelte";
     import Button from "../../lib/button.svelte";
 //   import type { PageData } from "../$types";
   
 //   export let data: PageData;
 
-  let todoList: string[] = [];
+    let todoList: string[] = [];
     let inputValue = '';
     $: disableStatus = inputValue.length < 2;
 
@@ -22,42 +22,39 @@
     })
   </script>
   
-  <div class="min-h-screen">
-      <h1 class="p-5 text-5xl font-light w-fit text-[#FF6000] block mx-auto">Taskmanager</h1>
-  
-      <div class="mx-8 my-4 md:block md:w-1/2 md:mx-auto text-xl">
-          <p class="leading-normal break-words text-center lg:text-left">
-              Lorem ipsum <span class="highlight">dolor sit amet</span> consectetur adipisicing elit. 
-              Aperiam porro, fugiat incidunt veniam labore modi tempore neque iure repellendus nemo illo 
-              quis maxime id natus debitis dignissimos quas! Suscipit, provident.
-          </p>
-          
-          <div class="flex justify-center md:block mt-5">
-              <input 
-              type="text"
-              id="todoInput" 
-              class="border border-[#FF6000] focus:(outline-none ring-2 ring-[#e65800])
-              rounded-lg p-1 text-lg block mb-2 w-[300px]" 
-              maxlength="25"
-              bind:value={inputValue}
-              autocomplete="off">
-              <Button on:click={addTask} width="300px" disabled={disableStatus}>
-                <div class="text-3xl i-ic:baseline-add mr-2" /> Add Task to your List 
-            </Button>
-          </div>
+<div class="min-h-screen">
+    <h1 class="p-5 text-5xl font-light w-fit text-[#FF6000] block mx-auto">Taskmanager</h1>
 
-          <table class="mt-13 w-full">
-            <thead>
-                <th class="p-3 text-left bg-gray-4">Tasks:</th>
-            </thead>
-            <tbody>
-                {#each todoList as todo}
-                    <tr class="odd:bg-gray-3 even:bg-gray-3/70 ">
-                        <td class="p-3">{todo}</td>
-                    </tr>
-                {/each}
-            </tbody>
-          </table>
+    <div class="mx-8 my-4 md:block md:w-1/2 md:mx-auto text-xl">
+        <p class="leading-normal break-words text-center lg:text-left">
+            Tasks are saved in your <span class="highlight">local storage!</span> If you want to remove the tasks you have to clear your local storage. 
+            Feel free to commit changes to the project. I created this svelte-project for <span class="highlight">testing purposes</span> only.
+        </p>
+        
+        <div class="flex justify-center md:block mt-5">
+            <input type="text" id="todoInput" 
+            class="border border-[#FF6000] focus:(outline-none ring-2 ring-[#e65800]) rounded-lg p-1 text-lg block mb-2 w-[300px]" 
+            maxlength="25" bind:value={inputValue} autocomplete="off">
 
-      </div>
+        <Button on:click={addTask} width="300px" disabled={disableStatus}>
+            <div class="text-3xl i-ic:baseline-add mr-2"/> Add Task to your List 
+        </Button>
+        </div>
+
+        <table class="mt-13 w-full">
+        <thead class="border-2 border-black">
+            <th class="p-3 text-left">Tasks:</th>
+        </thead>
+        <tbody>
+            {#each todoList as todo}
+                <tr class="odd:bg-gray-3 even:bg-gray-3/70 border-2 border-black">
+                    <td class="p-3">
+                        {todo}
+                    </td>
+                </tr>
+            {/each}
+        </tbody>
+        </table>
+
+    </div>
   </div>
